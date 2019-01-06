611d9da0cccc8e1636335113c6bf2b352acd7717
uurehman <uurehman.bscs15seecs@seecs.edu.pk>
2019-01-06 20:03:11 +0500

    dw
    wd

--- Insta/js/script.js
+++ Insta/js/script.js
@@ -1,12 +1,48 @@
 
+jQuery(document).ready(function() {
 
+//<!-- =============================================== -->
+//<!-- ========== scrollTop.js ========== -->
+//<!-- =============================================== -->
+    
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 600) {
+            $('.scrollup').fadeIn('slow');
+        } else {
+            $('.scrollup').fadeOut('slow');
+        }
+    });
+    $('.scrollup').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 800);
+        return false;
+    });
+			
+//<!-- =============================================== -->
+//<!-- ========== bootstrap scrollspy ========== -->
+//<!-- =============================================== -->
+    
+    
+    $('body').scrollspy({
+    	target: '.navbar',
+    	offset: 160
+    });
 
+//<!-- =============================================== -->
+//<!-- ========== navbardown.js ========== -->
+//<!-- =============================================== -->
 
+$('.nav a').bind('click', function () {
+    $('html , body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top - 80
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+});			
+			
+			
+
+ new WOW().init();
 
 
 		
-  
- 
-
-
-	
+		
+});
