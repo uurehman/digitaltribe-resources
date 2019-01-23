df479f41bea0c5e0735892846dc2757dd8e97078
uurehman <uurehman.bscs15seecs@seecs.edu.pk>
2019-01-23 21:09:22 +0500

    update

--- Insta/js/script.js
+++ Insta/js/script.js
@@ -1,60 +1,20 @@
 $(document).ready(function(){
-    var bodyWidth = $('body').width();
     var bglines = $('.bg-lines');
     addPadding($(bglines));
-    new WOW().init();      
 
     $(window).resize(function(){
         addPadding($(bglines));
     });
-    jssor_1_slider_init();
 });
 
 function addPadding($divClass) {
-    $bodyWidth = $('body').width();
+    var $bodyWidth = $('body').width();
     $padLeft = 0;
     if ($bodyWidth > 1280) {
         $padLeft = ($bodyWidth - 1280);
     }
     $divClass.css("margin-left", $padLeft);
+    var $headerGrad = $('.give_width').width();
+    $('.adapt_width').css("width", $headerGrad + 65);
+    console.log("Body width: " + $bodyWidth + ", Header-Grad: " + $headerGrad);
 }
-
-// PHONE SLIDER
-jssor_1_slider_init = function() {
-    var jssor_1_SlideshowTransitions = [
-      { $Duration: 500, $Delay: 80, $Cols: 8, $Rows: 4, $Clip: 15, $SlideOut: true, $Easing: $Jease$.$OutQuad },
-    ];
-    var jssor_1_options = {
-      $AutoPlay: 1,
-      $SlideshowOptions: {
-        $Class: $JssorSlideshowRunner$,
-        $Transitions: jssor_1_SlideshowTransitions,
-        $TransitionsOrder: 1
-      },
-      $ArrowNavigatorOptions: {
-        $Class: $JssorArrowNavigator$
-      },
-      $BulletNavigatorOptions: {
-        $Class: $JssorBulletNavigator$
-      }
-    };
-    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
-    /*#region responsive code begin*/
-    var MAX_WIDTH = 980;
-    function ScaleSlider() {
-        var containerElement = jssor_1_slider.$Elmt.parentNode;
-        var containerWidth = containerElement.clientWidth;
-        if (containerWidth) {
-            var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
-            jssor_1_slider.$ScaleWidth(expectedWidth);
-        }
-        else {
-            window.setTimeout(ScaleSlider, 30);
-        }
-    }
-    ScaleSlider();
-    $Jssor$.$AddEvent(window, "load", ScaleSlider);
-    $Jssor$.$AddEvent(window, "resize", ScaleSlider);
-    $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
-    /*#endregion responsive code end*/
-};
